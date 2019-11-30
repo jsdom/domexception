3b349e0817beff2cfa5c0c5dae8531c49c91760d
Pierre-Marie Dartus <p.dartus@salesforce.com>
2019-11-30 10:51:13 +0100

    implement feedback

--- test/test.js
+++ test/test.js
@@ -2,49 +2,78 @@
 const fs = require("fs");
 const path = require("path");
 const vm = require("vm");
-const { webidl2jsDOMException } = require("..");
+
 const testharness = require("./testharness.js");
+
+const SharedDOMException = require("../lib/index");
+const WrapperDOMException = require("../lib/webidl2js-wrapper");
 
 const wptDir = path.resolve(__dirname, "web-platform-tests");
 
-function createSandbox() {
-  const sandbox = Object.assign({ Error, Object, Function }, testharness);
-  webidl2jsDOMException.install(sandbox);
-
+function instantiateContext(sandbox) {
   vm.createContext(sandbox);
   sandbox.self = vm.runInContext("this", sandbox);
   return sandbox;
 }
 
-function runWPTFile(file) {
+function createSharedSandbox() {
+  const sandbox = Object.assign({ Error, Object, Function }, testharness);
+  Object.defineProperty(sandbox, "DOMException", {
+    value: SharedDOMException,
+    enumerable: false,
+    configurable: true,
+    writable: true
+  });
+  return sandbox;
+}
+
+function createWrapperSandbox() {
+  const sandbox = Object.assign({ Error, Object, Function }, testharness);
+  WrapperDOMException.install(sandbox);
+  return sandbox;
+}
+
+function runWPTFile(file, sandboxCreator) {
   const code = fs.readFileSync(file, "utf-8");
-  vm.runInContext(code, createSandbox(), {
+
+  const sandbox = sandboxCreator();
+  const context = instantiateContext(sandbox);
+
+  vm.runInContext(code, context, {
     filename: file,
     displayErrors: true
   });
 }
 
-describe("webidl2jsDOMException", () => {
+function runWPT(sandboxCreator) {
+  describe("Web platform tests", () => {
+    for (const file of fs.readdirSync(wptDir)) {
+      if (path.extname(file) === ".js") {
+        describe(file, () => {
+          runWPTFile(path.resolve(wptDir, file), sandboxCreator);
+        });
+      }
+    }
+  });
+}
+
+describe("Shared", () => {
+  runWPT(createSharedSandbox);
+});
+
+describe("webidl2js Wrapper", () => {
   it("throws when installing DOMException on a global object without an Error constructor", () => {
     testharness.assert_throws(new Error(), () => {
-      webidl2jsDOMException.install({});
+      WrapperDOMException.install({});
     });
   });
 
   it("throws when installing DOMException on a global object with an invalid Error constructor", () => {
     testharness.assert_throws(new Error(), () => {
       const Error = {};
-      webidl2jsDOMException.install({ Error });
+      WrapperDOMException.install({ Error });
     });
   });
-});
 
-describe("Web platform tests", () => {
-  for (const file of fs.readdirSync(wptDir)) {
-    if (path.extname(file) === ".js") {
-      describe(file, () => {
-        runWPTFile(path.resolve(wptDir, file));
-      });
-    }
-  }
+  runWPT(createWrapperSandbox);
 });
