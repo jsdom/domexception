20a586b1b16643a417d186a3af24285ad71eda44
Domenic Denicola <d@domenic.me>
2019-12-07 13:10:38 -0500

    Avoid circular dependency

--- webidl2js-wrapper.js
+++ webidl2js-wrapper.js
@@ -1,6 +1,16 @@
 "use strict";
+const DOMException = require("./lib/DOMException.js");
 
-const DOMException = require("./lib/DOMException");
-const { installOverride } = require("./lib/DOMException-impl");
+// Special install function to make the DOMException inherit from Error.
+// https://heycam.github.io/webidl/#es-DOMException-specialness
+function installOverride(globalObject) {
+  DOMException.install(globalObject);
+
+  if (typeof globalObject.Error !== "function") {
+    throw new Error("Internal error: Error constructor is not present on globalObject.");
+  }
+
+  Object.setPrototypeOf(globalObject.DOMException.prototype, globalObject.Error.prototype);
+}
 
 module.exports = Object.assign({}, DOMException, { install: installOverride });
