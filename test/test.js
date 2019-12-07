8960b5bc5e288209381fd2c6e19ded860cd61e0c
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2019-12-07 19:26:25 +0100

    Upgrade webidl2js
    This upgrades the package to generate the wrapper using the newest version of webidl2js, v11.0.0. It now additionally exposes the domexception/webidl2js-wrapper module, which can be used to install DOMException constructors on arbitrary global objects, using that global object's Error constructor. (This is useful for jsdom, after https://github.com/jsdom/jsdom/commit/5e39a4c60377c95ac09ea938ecbf5f1f91cb0360).
    
    Additional changes:
    
    * Raised minimum version to Node v8, so that we can use object spread syntax.
    * Updated all other dev dependencies.
    * Moved from "prepublish" to "prepare" scripts.

--- test/test.js
+++ test/test.js
@@ -2,39 +2,84 @@
 const fs = require("fs");
 const path = require("path");
 const vm = require("vm");
-const DOMException = require("..");
+const assert = require("assert");
+
 const testharness = require("./testharness.js");
+
+const SharedDOMException = require("..");
+const WrapperDOMException = require("../webidl2js-wrapper");
 
 const wptDir = path.resolve(__dirname, "web-platform-tests");
 
-function createSandbox() {
-  const sandbox = Object.assign({ Error, Object, Function }, testharness);
-  Object.defineProperty(sandbox, "DOMException", {
-    value: DOMException,
-    enumerable: false,
-    configurable: true,
-    writable: true
-  });
-
+function instantiateContext(sandbox) {
   vm.createContext(sandbox);
   sandbox.self = vm.runInContext("this", sandbox);
   return sandbox;
 }
 
-function runWPTFile(file) {
+function createSharedSandbox() {
+  const sandbox = Object.assign({ Error, Object, Function }, testharness);
+  Object.defineProperty(sandbox, "DOMException", {
+    value: SharedDOMException,
+    enumerable: false,
+    configurable: true,
+    writable: true
+  });
+  return sandbox;
+}
+
+function createWrapperSandbox() {
+  const sandbox = Object.assign({ Error, Object, Function }, testharness);
+  WrapperDOMException.install(sandbox);
+  return sandbox;
+}
+
+function runWPTFile(file, sandboxCreator) {
   const code = fs.readFileSync(file, "utf-8");
-  vm.runInContext(code, createSandbox(), {
+
+  const sandbox = sandboxCreator();
+  const context = instantiateContext(sandbox);
+
+  vm.runInContext(code, context, {
     filename: file,
     displayErrors: true
   });
 }
 
-describe("Web platform tests", () => {
-  for (const file of fs.readdirSync(wptDir)) {
-    if (path.extname(file) === ".js") {
-      describe(file, () => {
-        runWPTFile(path.resolve(wptDir, file));
-      });
+function runWPT(sandboxCreator) {
+  describe("Web platform tests", () => {
+    for (const file of fs.readdirSync(wptDir)) {
+      if (path.extname(file) === ".js") {
+        describe(file, () => {
+          runWPTFile(path.resolve(wptDir, file), sandboxCreator);
+        });
+      }
     }
-  }
+  });
+}
+
+describe("Shared", () => {
+  runWPT(createSharedSandbox);
+});
+
+describe("webidl2js Wrapper", () => {
+  it("throws when installing DOMException on a global object without an Error constructor", () => {
+    const badGlobal = {};
+    assert.throws(() => {
+      WrapperDOMException.install(badGlobal);
+    }, Error);
+
+    assert.equal("DOMException" in badGlobal, false);
+  });
+
+  it("throws when installing DOMException on a global object with an invalid Error constructor", () => {
+    const badGlobal = { Error: {} };
+    assert.throws(() => {
+      WrapperDOMException.install(badGlobal);
+    }, Error);
+
+    assert.equal("DOMException" in badGlobal, false);
+  });
+
+  runWPT(createWrapperSandbox);
 });
